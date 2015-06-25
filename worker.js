f103587acf7f2384b9b0d9630b8d102f756c9aa2
Darren Liew <ldarren@gmail.com>
2015-06-25 04:11:29 +0000

    working pico-common in server

--- worker.js
+++ worker.js
@@ -1,7 +1,14 @@
 var
-pico = require('pico-common')
+pico= require('pico-common'),
+utils= require('./lib/utils')
 
 pico.start({
+    name:'worker',
+    production:false,
+    ajax:utils.ajax,
+    paths:{
+        '*':'http://localhost/'
+    }
 },function(){
     console.log('hello')
 })
