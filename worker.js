b3bbc328e32acef0799e8e403dd21af69f71e09e
Darren Liew <ldarren@gmail.com>
2015-06-26 23:59:40 +0000

    working common on server side

--- worker.js
+++ worker.js
@@ -2,13 +2,18 @@ var
 pico= require('pico-common'),
 utils= require('./lib/utils')
 
-pico.start({
-    name:'worker',
-    production:false,
-    ajax:utils.ajax,
-    paths:{
-        '*':'http://localhost/'
-    }
-},function(){
-    console.log('hello')
-})
+    pico.start({
+        name:'worker',
+        production:false,
+        ajax:utils.ajax,
+        paths:{
+            '*':'http://localhost/'
+        }
+    },function(){
+        var hello=require('hello')
+console.log('loading...')
+        me.slot('load', function(){
+            console.log('loaded')
+            hello.said()
+        })
+    })
