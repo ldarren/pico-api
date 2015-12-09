2e4680dfe5b43f546ecb906a010737908dac1c51
Darren Liew <ldarren@gmail.com>
2015-12-09 08:22:26 +0000

    better cmd

--- mod/cmd.js
+++ mod/cmd.js
@@ -1,3 +1,6 @@
+const
+PROHIBITED=['process','Cmd','GLOBAL','global','root','require','module']
+
 var
 net = require('net'),
 repl = require('repl'),
@@ -6,38 +9,40 @@ args= require('../lib/args'),
 Session= require('../lib/Session'),
 inspectOpts={showHidden:false, depth:4, colors:true, customInspect:false},
 sigslot,
-// TODO: translate variables
-parse=function(args){
-    try{ return JSON.parse(`[${args.split(' ').join(',')}]`) }
-    catch(exp){this.write(exp.message)}
-    return null
+extendCtx=function(oldCtx, newCtx){
+    for(var i=0, k; k=PROHIBITED[i]; i++){
+        delete oldCtx[k]
+    }
+    Object.assign(oldCtx, newCtx)
 },
-render=function(cb, filename, context, next){
+render=function(input, next){
     if (this.has('error')){
-        cb(this.get('error'))
+        console.error(this.get('error'), this.getOutput())
         return next()
     }
     this.commit((err)=>{
         if (err) {
-            cb(err)
+            console.error(err)
             return next()
         }
-        cb(null, this.getOutput())
+        console.log(this.getOutput())
         next()
     })
 },
-// TODO: use Function object? signal become one of the context variable
-customEval=function(args, context, filename, cb){
-    var params=this.parseArgs(args)
-    console.log('###',params)
-    return cb(null, {hello:'world',foo:['bar',1,2,3]})
-    sigslot.signal(params[0], Session.TYPE.CMD, params[1], context, filename, cb, render)
+// adding indents with ellipses when inside blocks doesnt work, whose bug?
+customEval=function(statement, context, filename, cb){
+    var script=`with(context){return ${statement}}`
+
+    try{ var func=new Function('context','__filename','next',script) }
+    catch(exp){return}
+
+    return cb(null, func(context, filename))
 },
 customWriter=function(out){
     return util.inspect(out, inspectOpts)
 }
 Cmd=function(config, input, output){
-    var context={}
+    var ctx={}
     var svr=repl.start({
         input: input||process.stdin,
         output: output||process.stdout,
@@ -46,20 +51,23 @@ Cmd=function(config, input, output){
         useColors:config.useColors,
         useGlobal:false,
         ignoreUndefined:config.ignoreUndefined,
-        eval:customEval,
+        //eval:customEval, // custom eval not as good as default one
         writer: customWriter,
         replMode:repl.REPL_MODE_STRICT
     })
 
-    svr.parseArgs=parse
+    ctx['sigslot']=sigslot
+    ctx['Session']=Session
+    extendCtx(svr.context, ctx)
+
     this.server=svr
-    this.context=context
+    this.context=ctx
     svr.on('reset', (context)=>{
-        Object.assign(context, context)
-        sigslot.signal('cmd/reset', Session.TYPE.CMD, context)
+        extendCtx(context, ctx)
+        sigslot.signal('cmd/reset', Session.TYPE.CMD, context, render)
     })
     svr.on('exit', ()=>{
-        sigslot.signal('cmd/exit', Session.TYPE.CMD)
+        sigslot.signal('cmd/exit', Session.TYPE.CMD, 'sample data', render)
     })
 }
 
