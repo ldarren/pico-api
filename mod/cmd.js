784f27f1e4ea807183c9e91c219ec50a4b5cc471
Darren Liew <ldarren@gmail.com>
2015-12-09 04:10:45 +0000

    bug fixing

--- mod/cmd.js
+++ mod/cmd.js
@@ -1,9 +1,17 @@
 var
 net = require('net'),
 repl = require('repl'),
+util = require('util'),
 args= require('../lib/args'),
 Session= require('../lib/Session'),
+inspectOpts={showHidden:false, depth:4, colors:true, customInspect:false},
 sigslot,
+// TODO: translate variables
+parse=function(args){
+    try{ return JSON.parse(`[${args.split(' ').join(',')}]`) }
+    catch(exp){this.write(exp.message)}
+    return null
+},
 render=function(cb, filename, context, next){
     if (this.has('error')){
         cb(this.get('error'))
@@ -15,36 +23,54 @@ render=function(cb, filename, context, next){
             return next()
         }
         cb(null, this.getOutput())
+        next()
     })
 },
-customEval=function(cmd, context, filename, cb){
-    sigslot.signal(cmd, Session.TYPE.CMD, context, filename, cb, render)
+// TODO: use Function object? signal become one of the context variable
+customEval=function(args, context, filename, cb){
+    var params=this.parseArgs(args)
+    console.log('###',params)
+    return cb(null, {hello:'world',foo:['bar',1,2,3]})
+    sigslot.signal(params[0], Session.TYPE.CMD, params[1], context, filename, cb, render)
 },
-customWriter=function(){
-    return util.inspect(...arguments)
+customWriter=function(out){
+    return util.inspect(out, inspectOpts)
 }
 Cmd=function(config, input, output){
-    this.config=config
-    this.server=repl.start({
+    var context={}
+    var svr=repl.start({
         input: input||process.stdin,
         output: output||process.stdout,
         prompt: config.prompt,
         terminal:config.terminal,
         useColors:config.useColors,
-        useGlobal:config.useGlobal,
+        useGlobal:false,
         ignoreUndefined:config.ignoreUndefined,
         eval:customEval,
         writer: customWriter,
         replMode:repl.REPL_MODE_STRICT
     })
+
+    svr.parseArgs=parse
+    this.server=svr
+    this.context=context
+    svr.on('reset', (context)=>{
+        Object.assign(context, context)
+        sigslot.signal('cmd/reset', Session.TYPE.CMD, context)
+    })
+    svr.on('exit', ()=>{
+        sigslot.signal('cmd/exit', Session.TYPE.CMD)
+    })
 }
 
 Cmd.prototype={
-    defineCommand:function(key,action){
-        this.server.defineCommand(key, action)
+    updateContext:function(key, value){
+        this.context[key]=value
+        this.server.context[key]=value
     },
-    displayPrompt:function(preserveCursor){
-        this.server.displayPrompt(preserveCursor)
+    // e.g. key:hello cmd:{help:'say hello', action:function(params){this.write(params); this.displayPrompt}}
+    defineCommand:function(key,command){
+        this.server.defineCommand(key, command)
     }
 }
 
@@ -54,13 +80,14 @@ module.exports={
             socket:null,
             prompt: '> ',
             terminal:true,
-            useColors:false,
-            useGlobal:false,
+            useColors:true,
+            //useGlobal:false,
             ignoreUndefined:true
         }
 
         args.print('CMD Options',Object.assign(config,libConfig))
         sigslot= appConfig.sigslot
+        inspectOpts.colors=config.useColors
 
         if (!config.socket) return next(null, new Cmd(config))
         net.createServer((socket)=>{
