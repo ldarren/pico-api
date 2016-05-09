60cdb0b2293bb62e27e642cae5b79efa261c6fcb
Darren Liew <ldarren@gmail.com>
2016-05-09 10:57:40 +0000

    added redis pubsub

--- config/build/lead.data.sql
+++ config/build/lead.data.sql
@@ -5,15 +5,16 @@ INSERT INTO `hash` (`id`,`k`) VALUES
 (2,'pwd'),
 (3,'role'),
 (4,'consumer'),
-(5,'agent'),
-(6,'admin'),
-(7,'$place'),
-(8,'$case'),
-(9,'os'),
-(10,'token'),
-(11,'android'),
-(12,'ios'),
-(13,'passcode');
+(5,'applicant'),
+(6,'agent'),
+(7,'admin'),
+(8,'$place'),
+(9,'$case'),
+(10,'os'),
+(11,'token'),
+(12,'android'),
+(13,'ios'),
+(14,'passcode');
 /*
 INSERT INTO `lock` (`passcode`,`createdBy`) VALUES
 (64415253, 0);
--- mod/redis.js
+++ mod/redis.js
@@ -1,7 +1,19 @@
 // TODO: must contained pubsub communication channel, reuse pipeline feature?
 var
 redis = require('redis'),
-args= require('../lib/args')
+args= require('../lib/args'),
+Session= require('../lib/Session'),
+send=(sigslot,pattern,channel,msg)=>{
+	var input
+	try{input=JSON.parse(msg)}
+	catch(exp){input,msg}
+    sigslot.signal(pattern, Session.TYPE.REDIS,input,channel)
+},
+listenTo=(evt,client,channels,cb)=>{
+	if (!channels || !channels.length) return
+	client.on(evt,cb)
+	client.subscribe.apply(client, channels)
+}
 
 module.exports={
     create:function(appConfig, libConfig, next){
@@ -9,8 +21,8 @@ module.exports={
             host:'localhost',
             port:6379,
             database:0,
-            password:'null',
-            options:null,
+            password:null,
+            options:null
         }
 
         args.print('Redis Options',Object.assign(config,libConfig))
@@ -32,6 +44,14 @@ module.exports={
         client.on('connect', function(){
             console.log('redis conn[%s:%d.%d] connected',config.host,config.port,config.database)
         })
+
+		var sigslot=appConfig.sigslot
+		listenTo('message',client,config.subscribe,(channel,msg)=>{
+			send(sigslot,channel,channel,msg)
+		})
+		listenTo('pmessage',client,config.psubscribe,(pattern,channel,msg)=>{
+			send(sigslot,pattern,channel,msg)
+		})
         return next(null, client)
     }
 }
