273fb9d7d51349d969043e717dc02b8974dc1177
Darren Liew <ldarren@gmail.com>
2016-05-19 10:49:43 +0000

    refactoring again

--- config/build/lead.data.sql
+++ config/build/lead.data.sql
@@ -7,13 +7,11 @@ INSERT INTO `hash` (`id`,`k`) VALUES
 (4,'applicant'),
 (5,'agent'),
 (6,'admin'),
-(7,'$addr'),
 (8,'$detail'),
 (9,'os'),
 (10,'token'),
 (11,'android'),
 (12,'ios'),
 (13,'passcode'),
-(14,'$profile'),
 (15,'salt'),
 (16,'lockId');
