bb28f0946dfc7866ff3ee8ed5dc0a43b2dce22d7
Darren Liew <ldarren@gmail.com>
2017-05-20 17:10:38 +0000

    fixed app start in cluster mode

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ mods= require('./lib/mods'),
 cfg= require('./lib/cfg'),
 options=cfg.parse(__dirname)
 
-if (options && !options.master) return require('./lib/app')
+if (options && !options.app.master) return require('./lib/app')
 
 mods.load(options, (err, context)=>{
     if (err) return console.error(err)
