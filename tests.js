8bc116b203fe9f2c404deb25f565634531da21f7
Darren Liew <ldarren@gmail.com>
2018-04-14 08:57:33 +0000

    made api index and model index configurable

--- tests.js
+++ tests.js
@@ -1,15 +1,15 @@
-const pico=require('pico-common/bin/pico-cli')
-const ensure= pico.export('pico/test').ensure
+const pico =require('pico-common/bin/pico-cli')
+const { ensure } = pico.export('pico/test')
 const pUtil = require('picos-util')
 const cfg = require('./lib/cfg')
 const mods = require('./lib/mods')
 
 mods.load(cfg.parse(__dirname, {path: ['config/pico.test.json']}), (err, context) => {
 
-	ensure('ensure server is setup correctly', function(cb){
+	ensure('ensure server is setup correctly', cb => {
 		cb(err, context !== undefined)
 	})
-	ensure('ensure server is running correctly', function(cb){
+	ensure('ensure server is running correctly', cb => {
 		pUtil.ajax('GET', 'http://localhost:4888/pico', null, null, (err, state, res) => {
 			if (4 !== state) return
 			if (err) return cb(err, context !== undefined)
