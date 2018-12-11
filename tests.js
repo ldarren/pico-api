db2045870a318decd09d6e439bf4900fe615eaaa
Darren Liew <ldarren@gmail.com>
2018-12-11 02:42:44 +0000

    updated test

--- tests.js
+++ tests.js
@@ -1,13 +1,13 @@
 const pico =require('pico-common/bin/pico-cli')
-const { ensure } = pico.export('pico/test')
+const { test } = pico.export('pico/test')
 const pUtil = require('picos-util')
 const picos = require('./index')
 
-picos({path: ['config/pico.test.json'], master: [0]}, (err, ctx) => {
-	ensure('ensure server is setup correctly', cb => {
+picos({path: ['config/pico.test.json'], master: [false]}, (err, ctx) => {
+	test('ensure server is setup correctly', cb => {
 		cb(err, ctx !== undefined)
 	})
-	ensure('ensure server is running correctly', cb => {
+	test('ensure server is running correctly', cb => {
 		pUtil.ajax('GET', 'http://localhost:4888/pico', null, null, (err, state, res) => {
 			if (4 !== state) return
 			if (err) return cb(err, ctx !== undefined)
